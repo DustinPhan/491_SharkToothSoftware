fa9b3cba73c495f511a20c134d6ab37855e4f436
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-03-12 20:02:35 -0700

    Updated default page for site
    - Made the menupage the default page when going to the website.

--- Project/mysite/mysite/urls.py
+++ Project/mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("menuPage/" or "menupage/", include("menuPage.urls")),
+    path("", include("menuPage.urls")),
     path("admin/", admin.site.urls),
 ]
