e587655749db7308bffdc0de4453f74befb9a563
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-03-12 20:29:01 -0700

    Added redirect to home page
    - Changed url to redirect / to menuPage/

--- Project/mysite/menuPage/views.py
+++ Project/mysite/menuPage/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
-    print(request)
+def index(HttpRequest):
+    print(HttpRequest.path)
     return HttpResponse("<body><h1>test</h1></body>")
-
--- Project/mysite/restaurantSite/urls.py
+++ Project/mysite/restaurantSite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("menuPage/" or "menupage/", include("menuPage.urls")),
-    path("", include("menuPage.urls")),
+    path("", RedirectView.as_view(url='menuPage/', permanent=False)),
     path("admin/", admin.site.urls),
 ]
