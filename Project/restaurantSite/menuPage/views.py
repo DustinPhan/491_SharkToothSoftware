e31ef9d67d5552e43a9825fbad79b3f746d7f12d
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-03-14 20:27:30 -0700

    Change to use atlas
    - Changed database connection to use atlas instead of local

--- Project/restaurantSite/menuPage/views.py
+++ Project/restaurantSite/menuPage/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(HttpRequest):
     print(HttpRequest.path)
-    return HttpResponse("<body><h1>test</h1></body>")
+    #return HttpResponse("<head><title>" + HttpRequest.path[1:-1] + "</title></head><body><h1>test</h1></body>")
+    return render(HttpRequest, "index.html")
