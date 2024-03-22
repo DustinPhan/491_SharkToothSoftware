12826f9375d1f76501180689217ef393e7543fd6
Andrew Dutton <56041430+814207579@users.noreply.github.com>
2024-03-21 20:00:21 -0700

    Added frontend stuff
    - Added some front end
    - Added some view stuff

--- Project/restaurantSite/menuPage/views.py
+++ Project/restaurantSite/menuPage/views.py
@@ -6,4 +6,6 @@ from django.http import HttpResponse
 def index(HttpRequest):
     print(HttpRequest.path)
     #return HttpResponse("<head><title>" + HttpRequest.path[1:-1] + "</title></head><body><h1>test</h1></body>")
-    return render(HttpRequest, "index.html")
+    #always include the header
+    myArr = [1,2,3]
+    return render(HttpRequest, "index.html", {"array": myArr})
